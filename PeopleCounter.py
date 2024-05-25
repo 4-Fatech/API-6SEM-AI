08adb4e13d20c8fe337ee4a65c157448fcd1a646
Bruna <brunadias_30@hotmail.com>
2024-05-24 22:16:32 -0300

    feature: adicionei video 5 e melhorei logica para contagem

--- PeopleCounter.py
+++ PeopleCounter.py
@@ -5,7 +5,12 @@ from DatabaseHandler import DatabaseHandler
 
 
 class PeopleCounter:
-    def __init__(self, model_path="./runs/detect/train2/weights/best.pt", video_source='videos/video1.mp4', frame_skip=5):
+    def __init__(
+        self,
+        model_path="./runs/detect/train2/weights/best.pt",
+        video_source="videos/video1.mp4",
+        frame_skip=3,
+    ):
         self.model = YOLO(model_path)
         self.cap = dict()
         self.frame = None
@@ -15,7 +20,9 @@ class PeopleCounter:
         self.lotacao_atual = dict()
         self.classNames = ["person"]
         self.db_handler = DatabaseHandler()
-        self.frame_skip = frame_skip  # Número de frames a serem pulados entre processamentos
+        self.frame_skip = (
+            frame_skip  # Número de frames a serem pulados entre processamentos
+        )
 
         # # Verificar se a GPU está disponível
         # self.cuda_available = cv2.cuda.getCudaEnabledDeviceCount() > 0
@@ -27,7 +34,7 @@ class PeopleCounter:
     def run(self):
         list = self.db_handler.listRedzone()
         frame_count = 0  # Contador de frames
-        counter = 1
+        counter = 4
 
         for r in list.data:
             id = r["id_redzone"]
@@ -36,12 +43,12 @@ class PeopleCounter:
             self.pessoas_detectadas_saindo[id] = []
             self.cap[id] = cv2.VideoCapture(f"videos/video{counter}.mp4")
             counter += 1
-        
+
         while True:
             for r in list.data:
                 id = r["id_redzone"]
                 ret, frame = self.cap[id].read()
-                
+
                 if not ret:
                     break
 
@@ -63,7 +70,10 @@ class PeopleCounter:
                 # else:
                 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                results = self.model.track(frame, persist=True)
+                self.model(frame)
+                results = self.model.track(
+                    frame, persist=True, tracker="bytetrack.yaml"
+                )
                 for r in results:
                     boxes = r.boxes
                     for box in boxes:
@@ -72,8 +82,9 @@ class PeopleCounter:
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
-        self.cap.release()
-        cv2.destroyAllWindows()
+        self.cap[id].release()
+        cv2.destroyWindow(str(id))
+        # cv2.destroyAllWindows()
 
     def process_box(self, box, class_names, frame, redzone):
         x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -85,29 +96,69 @@ class PeopleCounter:
         if cls < len(class_names) and class_names[cls] == "person" and acuracia >= 0.3:
             cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
             if x1 + w // 2 > self.line_x - 200 and x1 + w // 2 < self.line_x + 200:
-                if (x1 + w // 2 < self.line_x and box.id not in self.pessoas_detectadas_entrando[redzone]):
+                if (
+                    x1 + w // 2 < self.line_x
+                    and box.id not in self.pessoas_detectadas_entrando[redzone]
+                ):
                     self.pessoas_detectadas_entrando[redzone].append(box.id)
-                if (x1 + w // 2 > self.line_x and box.id not in self.pessoas_detectadas_saindo[redzone]):
+                if (
+                    x1 + w // 2 > self.line_x
+                    and box.id not in self.pessoas_detectadas_saindo[redzone]
+                ):
                     self.pessoas_detectadas_saindo[redzone].append(box.id)
-                if x1 + w // 2 > self.line_x and box.id in self.pessoas_detectadas_entrando[redzone]:
+                if (
+                    x1 + w // 2 > self.line_x
+                    and box.id in self.pessoas_detectadas_entrando[redzone]
+                ):
                     self.pessoas_detectadas_entrando[redzone].remove(box.id)
                     self.lotacao_atual[redzone] += 1
-                    self.db_handler.insert_record(True, self.lotacao_atual[redzone], redzone)
-                if x1 + w // 2 < self.line_x and box.id in self.pessoas_detectadas_saindo[redzone]:
+                    self.db_handler.insert_record(
+                        True, self.lotacao_atual[redzone], redzone
+                    )
+                if (
+                    x1 + w // 2 < self.line_x
+                    and box.id in self.pessoas_detectadas_saindo[redzone]
+                ):
                     self.pessoas_detectadas_saindo[redzone].remove(box.id)
                     if self.lotacao_atual[redzone] != 0:
                         self.lotacao_atual[redzone] -= 1
-                        self.db_handler.insert_record(False, self.lotacao_atual[redzone], redzone)
-            text = f'Class: {class_names[cls]}, Acuracia: {acuracia:.2f}'
-            cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        self.db_handler.insert_record(
+                            False, self.lotacao_atual[redzone], redzone
+                        )
+            text = f"Class: {class_names[cls]}, Acuracia: {acuracia:.2f}"
+            cv2.putText(
+                frame,
+                text,
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2,
+            )
 
     def display(self, frame, redzone):
         if frame is not None:
             height, width, _ = frame.shape
-            cv2.line(frame, (self.line_x, 0), (self.line_x, height), (0, 255, 0), 2)
-            cv2.putText(self.frame,f"Lotacao Atual: {self.lotacao_atual}",(10, 50),cv2.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0),2)
-            cv2.putText(frame, f"Redzone: {redzone}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.imshow("frame" + str(redzone), frame)
+            # cv2.line(frame, (self.line_x, 0), (self.line_x, height), (0, 255, 0), 2)
+            cv2.putText(
+                frame,
+                f"Redzone ID: {redzone}",
+                (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+            )
+            cv2.putText(
+                frame,
+                f"Lotacao Atual: {self.lotacao_atual[redzone]}",
+                (10, 70),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+            )
+            cv2.imshow(winname=str(redzone), mat=frame)
 
 
 if __name__ == "__main__":
